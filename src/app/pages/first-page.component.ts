e3884566eae441aefc955e627a38d0778cc196be
Lourival Nunes <Lourival.nunes88@gmail.com>
2024-06-01 12:26:04 -0300

    foi criado uma lista onde tem um input text que faça o bind para uma variavel, e ao  clicar no bat

--- src/app/pages/first-page.component.ts
+++ src/app/pages/first-page.component.ts
@@ -7,8 +7,23 @@ import { Component } from "@angular/core";
 export class FirstPageComponent {
     btnDisabled = false
     name ='Teste de alerta' 
+    botaoClicado = false
+    novoItem = ''; //nova variavel para armazenar o valor do input
 
     mostrarValor(){
         alert(this.name)
     }
+
+    trocarBotao(){
+        this.botaoClicado = ! this.botaoClicado;
+    }
+    
+    adicionarItem(){
+        if(this.novoItem){
+            this.items.push({name: this.novoItem});
+            this.novoItem = ''; // limpar o inut após adicionar
+        }
+    }
+    
+    items = [{name:'teste'}, {name:'outro item'}]
 }
